621de5fe64f89dc3300a655f13206681f92c575f
antonukom@gmail.com <antonukom@gmail.com>
2016-07-21 14:13:40 +0300

    fixed title search method

--- Brights.TestTask/Controllers/ParseController.cs
+++ Brights.TestTask/Controllers/ParseController.cs
@@ -1,6 +1,10 @@
 ﻿using Brights.TestTask.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Web.Mvc;
 
 namespace Brights.TestTask.Controllers
@@ -40,31 +44,65 @@ namespace Brights.TestTask.Controllers
         /// <param name="urlString">Except any string</param>
         /// <returns>UrlModel or null if parse fail</returns>
         private UrlModel GetModel(string urlString)
-        {           
+        {
             try
-            {                
+            {
                 //get uri from arrived string
                 var uri = new Uri(urlString);
 
                 var model = new UrlModel();
                 //set required url
                 model.Url = uri.Scheme + @"://" + uri.Host;
-
-                //find title
-                string[] hostArray = uri.Host.Split('.');
-                int index = 0;
-                                
-                if (hostArray.Length > 2)
-                {
-                    index = 1;
-                }
-                //set title and make first letter uppercase
-                model.Title = char.ToUpper(hostArray[index][0]) + hostArray[index].Substring(1);
+                model.Title = GetWebPageTitle(urlString);
                 return model;
             }
             catch
             {
                 return null;
+            }
+        }
+
+        private string GetWebPageTitle(string url)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest.Create(url) as HttpWebRequest);
+                HttpWebResponse response = (request.GetResponse() as HttpWebResponse);
+
+                using (Stream stream = response.GetResponseStream())
+                {
+                    // regexp to check for <title></title> block
+                    Regex titleCheck = new Regex(@"<title>\s*(.+?)\s*</title>",
+                        RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+                    //buffer max size 
+                    int bytesToRead = 5000;
+                    byte[] buffer = new byte[bytesToRead];
+                    
+                    //fill buffer with response data
+                    stream.Read(buffer, 0, bytesToRead);
+
+                    string contents = Encoding.UTF8.GetString(buffer, 0, bytesToRead);
+
+                    Match match = titleCheck.Match(contents);
+
+                    string title = string.Empty;
+                    if (match.Success)
+                    {
+                        // found a <title></title> match
+                        title = match.Groups[1].Value.ToString();
+                    }
+                    else if (contents.Contains("</head>") || string.IsNullOrWhiteSpace(title))
+                    {
+                        //no title found
+                        title = "[Tag 'title' wasn't found in current URL]";
+                    }
+                    return title;
+                }
+            }
+            catch
+            {
+                return "Failed to find title of current URL";
             }
         }
     }
